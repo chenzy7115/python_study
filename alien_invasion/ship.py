12444fae7e008192a5ebae8f2d20598f42435259
chenzy7115 <swczy@126.com>
2026-01-12 14:30:24 +0800

    display&moveing ship

---
+++ alien_invasion/ship.py
@@ -0,0 +1,43 @@
+import pygame
+
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        # 设置飞船的初始位置为屏幕底部中央
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # will int change float.
+        self.x = float(self.rect.x)
+        
+        # moveing flag.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
+        # 检查飞船是否位于屏幕边缘，以防止它移动到屏幕外面。
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+   
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+            
+        # Update rect object from self.x.
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
---
+++ alien_invasion/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+            sys.exit()
