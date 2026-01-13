836ccfc1536dcf5ec223c30960ebca57c73c40bc
chenzy7115 <swczy@126.com>
2026-01-13 16:54:08 +0800

    group collide

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -52,6 +52,8 @@ class AlienInvasion:
             # Redraw the screen during each pass through the loop.
             self._update_screen()
 
+            self._update_aliens()
+
             # # Make the most recently drawn screen visible.
             # pygame.display.flip()
 
@@ -67,6 +69,9 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+        # Check for any bullets that have hit aliens.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, False, True)
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -111,10 +116,12 @@ class AlienInvasion:
 
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
-        
+
         # Create an alien and find the number of rows of aliens that fit on the screen.
         ship_height = self.ship.rect.height
-        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        available_space_y = (
+            self.settings.screen_height - (3 * alien_height) - ship_height
+        )
         number_rows = available_space_y // (2 * alien_height)
 
         # create the first row of aliens.
@@ -131,6 +138,24 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -17,3 +17,8 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+        # alien settings
+        self.alien_speed = 0.2
+        self.fleet_drop_speed = 3
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
